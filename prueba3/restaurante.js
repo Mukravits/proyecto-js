c2d42d33fa49ba5dbbd809febc778e7b2030be74
Mauricio Hernandez <lic.mauriciodobleh@gmail.com>
2023-12-27 11:51:53 -0600

    update

--- prueba3/restaurante.js
+++ prueba3/restaurante.js
@@ -4,5 +4,5 @@ var totalbebida = 900 + 650;
 var total = totalcomida + totalbebida;
 
 console.log("El total de comida es: " + totalcomida);
-console.log("El total de bebida es: " + totalcomida);
+console.log("El total de bebida es: " + totalbebida);
 console.log("El total de la cuenta es: " = total);
