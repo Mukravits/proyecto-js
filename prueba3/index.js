89b8cc4bda8c638c07019a4c7df39ff9f19ff473
Mauricio Hernandez <lic.mauriciodobleh@gmail.com>
2023-12-29 06:57:50 -0600

    updates

---
+++ prueba3/index.js
@@ -0,0 +1,19 @@
+// var totalcomida = 2800 + 1300;
+// var totalbebida = 900 + 650;
+
+// var total = totalcomida + totalbebida;
+
+// console.log("El total de comida es: " + totalcomida);
+// console.log("El total de bebida es: " + totalbebida);
+// console.log("El total de la cuenta es: " + total);
+
+// ejemplo de if & else 
+
+var aprobe = true;
+
+if (aprobe == true) {
+    console.log("puedo salir");
+}
+else {
+    console.log("tengo que seguir estudiando");
+}
--- prueba3/restaurante.js
+++
@@ -1,8 +0,0 @@
-var totalcomida = 2800 + 1300;
-var totalbebida = 900 + 650;
-
-var total = totalcomida + totalbebida;
-
-console.log("El total de comida es: " + totalcomida);
-console.log("El total de bebida es: " + totalbebida);
-console.log("El total de la cuenta es: " + total);
