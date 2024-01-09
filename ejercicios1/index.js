759ebf3509d0496666608ed3ce464f088ea43056
Mauricio Hernandez <lic.mauriciodobleh@gmail.com>
2024-01-08 22:26:56 -0600

    update

--- ejercicios1/index.js
+++ ejercicios1/index.js
@@ -1,14 +1,20 @@
 
 // ejercio de matriz para localizar un determinado objeto
-//ejemplo con herramientas
+//ejemplo con herramientas, MI primer algoritmo!
 
-let matrizH = new Array (3);
 
 let Tool0 = ["martillo", "pinzas", "machete"];
 let Tool1 = ["desarmador", "taladro", "navaja"];
 let Tool2 = ["cinta", "tijeras", "mazo"];
 
+    let matrizH = [Tool0, Tool1, Tool2];
+
+    for (let i=0; i<matrizH; i++) {
+     console.log(matrizH[i]);
+
+    } 
     console.log (matrizH);
+        
 
 
 
